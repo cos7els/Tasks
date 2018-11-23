4783ac5f140235c3b88a299ea71ed0a20452d6c7
Alexander Mazur <cos7els@gmail.com>
2018-11-23 12:44:25 +0300

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -3,7 +3,6 @@ import java.io.Serializable;
 public class Person implements Serializable {
     private String name;
     private int age;
-
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
